c90000b1f30db548e356989557ae004829dfb4c8
Marika Jarosz <marika@MacBook-Air-Marika.local>
2019-03-28 13:15:26 +0100

    First version

---
+++ Assets/_Completed-Game/Scripts/time.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public Time;
+private float secondsCount;
+private int minuteCount;
+
+void Update()
+{
+    UpdateTimerUI();
+}
+
+public void UpdateTimerUI()
+{
+
+    timerText.text = minuteCount.ToString("00") + "m:" + ((int)secondsCount).ToString("00") + "s";
+
+}
+}
